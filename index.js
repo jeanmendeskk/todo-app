630c24fc801c28b8691f5f24fe0e4f4edbd1a905
Jean Mendes <jeanmds7809@gmail.com>
2023-11-26 00:43:37 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (require, response) => {
+    response.send("Olá, Mundo")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
