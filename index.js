90563b56dcf406a9537396f24ba3279d6b2dee01
Jean Mendes <jeanmds7809@gmail.com>
2023-11-30 04:47:14 -0300

    filtro de tarefas completas, completo

--- index.js
+++ index.js
@@ -69,6 +69,31 @@ app.post('/criar', (require, response) => {
     })
 })
 
+app.get('/completas', (require, response) => {
+    const sql = `
+        SELECT * FROM tarefas
+        WHERE completa = 1
+    `
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: true
+            }
+        })
+
+        const quantidadeTarefas = tarefas.length
+
+        response.render('completas', {tarefas, quantidadeTarefas})
+    })
+})
+
 app.get('/ativas', (require, response) => {
     const sql = `
         SELECT * FROM tarefas
